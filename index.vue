c87c9551ec347ae91a8283b4aee8d0db1e300a83
dudu-chao <duduchao298@gmail.com>
2025-03-21 08:51:56 +0800

    index.vue

--- index.vue
+++ index.vue
@@ -1,52 +1,43 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
-	</view>
+  <view class="container">
+    <text class="title">欢迎来到首页</text>
+    <button class="blockly-btn" type="primary" @click="openBlocklyEditor">
+      打开 Blockly 编辑器
+    </button>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+export default {
+  methods: {
+    openBlocklyEditor() {
+      // 使用 uni.navigateTo 跳转到 Blockly 页面
+      uni.navigateTo({
+        url: '/pages/blockly/blockly'
+      });
+    }
+  }
+};
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+.title {
+  font-size: 20px;
+  color: #333;
+  margin-bottom: 20rpx;
+}
+.blockly-btn {
+  margin: 20rpx;
+  padding: 20rpx;
+  background-color: #27ae60;
+  color: #fff;
+  border-radius: 10rpx;
+}
 </style>
